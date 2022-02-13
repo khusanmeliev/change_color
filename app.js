ab8a322b1154cb7a3200bb7de904b31a5572517c
javokhirm <javokhirmeliev@gmail.com>
2022-02-13 22:10:40 +0500

    color

---
+++ app.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+});
+
+
